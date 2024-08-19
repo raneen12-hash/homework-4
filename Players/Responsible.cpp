e1cb45ef00e299617726052319c72cdf6bdba02f
Haj Yahya Raneen <raneen.haj@campus.technion.ac.il>
2024-08-19 21:06:57 +0300

    Update Responsible.cpp

--- Players/Responsible.cpp
+++ Players/Responsible.cpp
@@ -4,8 +4,11 @@
 Responsible::Responsible(string type) : m_type(type){}
 
 void Responsible::buy(Player& player){
-    int counter = 0;
-    while( (player.getHealthPoints() <= 100) && player.pay(5)){
+    int counter = 0,temp=100;
+    if(player.getjob()=="Warrior"){
+        temp=150;
+    }
+    while( (player.getHealthPoints() < temp) && player.pay(5)){
         player.heal(10);
         counter++;
     }
